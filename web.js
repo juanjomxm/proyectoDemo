f55db36d910d9731c82494b6f63dac0d8490d46e
juanjo314 <juanchi_z_29@hotmail.com>
2023-02-17 16:55:26 -0500

    añadiendo seccion escoger auto(constancia)

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 
-    const boton = document.querySelector("button")
-    boton.addEventListener("click", nombreYApellido)
+const boton = document.querySelector("button")
+boton.addEventListener("click", nombreYApellido)
+
+const botonAuto = document.querySelector("#auto-elegido")
+botonAuto.addEventListener("click", seleccionarAuto)
 
 
 
@@ -8,7 +11,28 @@ function nombreYApellido(){
     const input = document.querySelector("#nombre")
     const input2 = document.querySelector("#apellido")
     const p = document.querySelector("#resultado")
-    const suma ="Tu nombre es: " + input.value + " " + input2.value
+    const suma ="Bienvenido(a) " + input.value + " " + input2.value
     p.innerHTML = suma
+
+}
+
+function seleccionarAuto() {
+    const inputAuto = document.querySelector("#ferrari")
+    const inputAuto2 = document.querySelector("#lamborghini")
+    const inputAuto3 = document.querySelector("#bugatti")
+    const inputAuto4 = document.querySelector("#tesla")
+    const pAuto = document.querySelector("#auto-escogido")
+
+    if(inputAuto.checked){
+        pAuto.innerHTML = "Elegiste un Ferrari"
+    } else if(inputAuto2.checked){
+        pAuto.innerHTML = "Elegiste un Lamborghini " 
+    } else if(inputAuto3.checked){
+        pAuto.innerHTML = "Elegiste un Bugatti"
+    } else if(inputAuto4.checked){
+        pAuto.innerHTML = "Elegiste un Tesla" 
+    } else {
+        pAuto.innerHTML = "Escoge algun auto"
+    }
 
 }
